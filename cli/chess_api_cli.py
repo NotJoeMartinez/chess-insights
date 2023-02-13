3ec07b13d8ac14e707eba99625a5d57c62e24aac
home <home@DESKTOP.local>
2023-02-12 23:02:19 -0600

    cli updates

--- cli/chess_api_cli.py
+++ cli/chess_api_cli.py
@@ -1,4 +1,5 @@
 import requests, json, re, datetime, sys 
+import sqlite3
 
 def main():
     arg_one = sys.argv[1]
@@ -16,7 +17,8 @@ def main():
             exit()
         else:
             json_path = sys.argv[2]
-            get_stats(json_path)
+            username = sys.argv[3]
+            get_stats(json_path, username)
     else:
         print(f"Argument [{arg_one}] not recognized")
 
@@ -25,9 +27,11 @@ def get_games(user):
     r = requests.get(f"https://api.chess.com/pub/player/{user}/games/archives")
     month_urls = r.json()
 
+
     games_dict = {}
 
     for url in month_urls["archives"]:
+        res = requests.get(url).json()
         match = re.search(r"games\/(\d{4})\/(\d{2})$",url)
         year = match.group(1)
         month = match.group(2)
@@ -50,12 +54,102 @@ def get_games(user):
 
     print(f"Saved data from {user} to {fname}")
     
+def parse_pgn(pgn):
+    parsed_pgn = {}
 
-def get_stats(json_path):
+    pgn = pgn.split("\n")
+
+    date = re.sub(r"\s|Date|\[|\]|\"|","",pgn[2])
+    opening_url = re.sub(r"\s|\[ECOUrl|\]|\"|","",pgn[10])
+    start_time = re.sub(r"\s|\[StartTime|\]|\"","",pgn[17])
+    end_time = re.sub(r"\s|\[EndTime|\]|\"","",pgn[19])
+
+    opening_group = re.search("openings/(.*)$",opening_url).group(1)
+    opening = re.sub("-"," ",opening_group)
+
+    parsed_pgn["date"] = date
+    parsed_pgn["opening_url"] = opening_url
+    parsed_pgn["opening"] = opening
+    parsed_pgn["start_time"] = start_time
+    parsed_pgn["end_time"] = end_time
+
+    return parsed_pgn
+
+def parse_game_node(game, uname):
+    parsed = {}
+    parsed["user_accuracy"] = ""
+    parsed["opponent_accuracy"] = ""
+
+    parsed["game_url"] = game["url"]
+    parsed["game_id"] = re.search(r"live|daily/(.*)$",game["url"]).group(1)
+    parsed["pgn"] = game["pgn"]
+    parsed["time_class"] = game["time_class"]
+    parsed["fen"] = game["fen"]
+
+    if (game["white"]["username"] == uname):
+        parsed["user_color"] = "white"
+        parsed["user_rating"] = game["white"]["rating"]
+        parsed["opponent"] = game["black"]["username"]
+        parsed["opponent_rating"] = game["black"]["rating"]
+        parsed["opponent_url"] = "https://www.chess.com/member/" + game["black"]["username"]
+        parsed["result"] = game["white"]["result"]
+        # check for accuracies 
+        if "accuracies" in game: 
+            parsed["user_accuracy"] = game["accuracies"]["white"]
+            parsed["opponent_accuracy"] = game["accuracies"]["black"]
+
+    else:
+        parsed["user_color"] = "black"
+        parsed["user_rating"] = game["black"]["rating"]
+        parsed["opponent"] = game["white"]["username"]
+        parsed["opponent_rating"] = game["white"]["rating"]
+        parsed["opponent_url"] = "https://www.chess.com/member/" + game["white"]["username"]
+        parsed["result"] = game["black"]["result"]
+        # check for accuracies 
+        if "accuracies" in game: 
+            parsed["user_accuracy"] = game["accuracies"]["black"]
+            parsed["opponent_accuracy"] = game["accuracies"]["white"]
+
+    parsed_pgn = parse_pgn(game["pgn"])
+    parsed["date"] = parsed_pgn["date"]
+    parsed["opening_url"] = parsed_pgn["opening_url"]
+    parsed["opening"] = parsed_pgn["opening"]
+    parsed["start_time"] = parsed_pgn["start_time"]
+    parsed["end_time"] = parsed_pgn["end_time"]
+
+    return parsed
+
+def get_stats(json_path, username):
 
     with open(json_path, "r") as f:
         data = json.load(f)
 
+
+    sql = """CREATE TABLE IF NOT EXISTS games (
+                user_accuracy TEXT, 
+                opponent_accuracy TEXT, 
+                game_url TEXT, 
+                game_id TEXT, 
+                pgn TEXT, 
+                time_class TEXT, 
+                fen TEXT, 
+                user_color TEXT, 
+                user_rating TEXT, 
+                opponent TEXT, 
+                opponent_rating TEXT, 
+                opponent_url TEXT, 
+                result TEXT, 
+                date TEXT, 
+                opening_url TEXT, 
+                opening TEXT, 
+                start_time TEXT, 
+                end_time TEXT);
+          """
+    conn = sqlite3.connect(f"{username}.db")
+    cur = conn.cursor()
+    cur.execute(sql)
+
+    rows = []
     # lol
     for year in data.keys():
         months_lst = data[year]
@@ -64,11 +158,14 @@ def get_stats(json_path):
                 games_dict = month_dict[month]
                 for games in games_dict:
                     for game in games_dict[games]:
-                        print(game["url"])
-    
+                        node = parse_game_node(game, username)
+                        print(node["game_id"])
+                        row = tuple(list(node.values()))
+                        rows.append(row)
 
-
-
+    # this seems normal
+    cur.executemany("INSERT INTO games VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", rows)
+    conn.commit()  
 
 
 if __name__ == '__main__': 
