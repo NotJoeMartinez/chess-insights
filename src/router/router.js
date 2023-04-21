b1fb8b33b1412322bedd8fbad337ade1b299b4e6
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2023-04-20 23:49:50 -0500

    moved homePage to pages dir

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomePage from '@/components/HomePage.vue'
+import HomePage from '@/pages/HomePage.vue'
 
 const routes = [
     {'path': '/', component: HomePage},
